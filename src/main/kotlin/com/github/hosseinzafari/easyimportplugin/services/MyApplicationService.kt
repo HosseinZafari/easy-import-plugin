ca1e7840c3523efa7df376a9c6f78e754d662909
GitHub Action <action@github.com>
2021-07-20 18:00:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hosseinzafari/easyimportplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hosseinzafari.easyimportplugin.services
+
+import com.github.hosseinzafari.easyimportplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
